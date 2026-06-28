2fc082ec7a4bff4f845856756e32f463075cca25
rafaelcarvalho-git <rafaskyplay@gmail.com>
2026-06-28 16:11:33 -0300

    implementando navbar

---
+++ src/app/shared/components/nav/nav.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.html',
+  styleUrl: './nav.scss',
+})
+export class Nav {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
