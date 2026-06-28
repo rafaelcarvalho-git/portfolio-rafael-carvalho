3b6f2074859948a761f2e53001eb2b874256b012
rafaelcarvalho-git <rafaskyplay@gmail.com>
2026-06-28 16:30:22 -0300

    novo prototipo

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { About } from '../../shared/about/about';
 import { ProfessionalExperience } from '../../shared/professional-experience/professional-experience';
 import { Skills } from '../../shared/skills/skills';
 import { Footer } from '../../shared/components/footer/footer';
+import { Nav } from '../../shared/components/nav/nav';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, About, ProfessionalExperience, Skills, Footer],
+  imports: [Header, About, ProfessionalExperience, Skills, Footer, Nav],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
